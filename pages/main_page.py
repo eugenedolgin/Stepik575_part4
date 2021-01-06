42b1455410534cc0dfdeb6ea8664abec0ff14a72
eugenedolgin <eugenedolgin@gmail.com>
2021-01-06 23:44:57 +0400

    Move Open Login page to BasePage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,15 +1,13 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
-from selenium.webdriver.common.by import By
-
-from .login_page import LoginPage
+# from .locators import MainPageLocators
+# from selenium.webdriver.common.by import By
+# from .login_page import LoginPage
 
 class MainPage(BasePage): 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
-        #return LoginPage(browser=self.browser, url=self.browser.current_url) # Method One
-        
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
+        # Конструктор с ключевым словом super вызывает конструктор класса предка 
+        # и передает ему все те аргументы, которые мы передали в конструктор MainPage
+
         
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .pages.product_page import ProductPage
 
+@pytest.mark.skip
 def _test_guest_can_add_product_to_basket(browser):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -8,19 +9,34 @@ def _test_guest_can_add_product_to_basket(browser):
     prod_page.open()                         # открываем страницу
     prod_page.run_test_product()             # выполняем метод страницы — кладем в корзину
 
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+@pytest.mark.skip
+def _test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     prod_page = ProductPage(browser, link)   
     prod_page.open()
     prod_page.do_click_add_to_basket()
     prod_page.should_not_be_success_message()
-    
+
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     prod_page = ProductPage(browser, link)   
     prod_page.open()
     prod_page.should_not_be_success_message()
-    
+
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     prod_page = ProductPage(browser, link)   
@@ -39,7 +55,8 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def _test_guest_can_add_product_to_basket(browser,link):
+@pytest.mark.skip
+def test_guest_can_add_product_to_basket(browser,link):
     prod_page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     prod_page.open()                         # открываем страницу
     prod_page.run_test_product()             # выполняем метод страницы — кладем в корзину
